19cdcf1d0b720ed08012388f2b631a382b7fa18f
SmokeyStack <seansyahril04@gmail.com>
2022-11-20 17:49:18 -0500

    Initial commit

---
+++ SmokeyVersionSwitcher/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace SmokeyVersionSwitcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Debug.Listeners.Add(new TextWriterTraceListener("Log.txt"));
+            Debug.AutoFlush = true;
+        }
+    }
+}
---
+++ SmokeyVersionSwitcher/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SmokeyVersionSwitcher
+{
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Threading;
+
+    public class Foo
+    {
+        public string Section { get; set; }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Foo> _versions;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _versions = new List<Foo>();
+            _versions.Add(new Foo() { Section = "Chiang" });
+            BetaVersionList.DataContext = _versions;
+        }
+    }
+}
