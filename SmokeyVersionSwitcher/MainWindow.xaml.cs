27136d70db13fc36668dfd60ccd0afa41c62c03a
SmokeyStack <seansyahril04@gmail.com>
2022-11-20 19:21:03 -0500

    Parsed versions.json

--- SmokeyVersionSwitcher/MainWindow.xaml.cs
+++ SmokeyVersionSwitcher/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,11 +14,6 @@ namespace SmokeyVersionSwitcher
     using System.Diagnostics;
     using System.Threading;
 
-    public class Foo
-    {
-        public string Section { get; set; }
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -26,8 +24,29 @@ namespace SmokeyVersionSwitcher
         {
             InitializeComponent();
             _versions = new List<Foo>();
-            _versions.Add(new Foo() { Section = "Chiang" });
-            BetaVersionList.DataContext = _versions;
+            //_versions.Add(new Foo() { Section = "Chiang" });
+            //BetaVersionList.DataContext = _versions;
+            try
+            {
+                //JsonSerializer serializer = new JsonSerializer();
+                //Foo foo = (Foo)serializer.Deserialize(reader, typeof(Foo));
+                JArray jArray = JsonConvert.DeserializeObject<JArray>(File.ReadAllText("versions.json"));
+                foreach (JObject keys in jArray)
+                {
+                    MessageBox.Show((string)keys["Name"]);
+                    _versions.Add(new Foo() { Name = (string)keys["Name"], Type = (string)keys["Type"] });
+                }
+                VersionList.DataContext = _versions;
+            }
+            catch (Exception)
+            {
+            }
         }
+    }
+
+    public class Foo
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
     }
 }
