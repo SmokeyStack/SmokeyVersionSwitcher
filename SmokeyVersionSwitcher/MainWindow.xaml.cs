1b0e6147d386cf2887c9c4439e4eca29e3a9ace2
SmokeyStack <seansyahril04@gmail.com>
2022-11-20 21:17:15 -0500

    Implemented buttons

--- SmokeyVersionSwitcher/MainWindow.xaml.cs
+++ SmokeyVersionSwitcher/MainWindow.xaml.cs
@@ -1,11 +1,6 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace SmokeyVersionSwitcher
@@ -13,40 +8,178 @@ namespace SmokeyVersionSwitcher
     using System.ComponentModel;
     using System.Diagnostics;
     using System.Threading;
+    using WPFDataTypes;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IVersionCommands
     {
-        private List<Foo> _versions;
+        private readonly VersionList _versions;
+
         public MainWindow()
         {
             InitializeComponent();
-            _versions = new List<Foo>();
-            //_versions.Add(new Foo() { Section = "Chiang" });
-            //BetaVersionList.DataContext = _versions;
-            try
+            _versions = new VersionList("versions.json", this);
+            VersionList.DataContext = _versions;
+            Dispatcher.Invoke(async () =>
             {
-                //JsonSerializer serializer = new JsonSerializer();
-                //Foo foo = (Foo)serializer.Deserialize(reader, typeof(Foo));
-                JArray jArray = JsonConvert.DeserializeObject<JArray>(File.ReadAllText("versions.json"));
-                foreach (JObject keys in jArray)
+                try
                 {
-                    MessageBox.Show((string)keys["Name"]);
-                    _versions.Add(new Foo() { Name = (string)keys["Name"], Type = (string)keys["Type"] });
+                    await _versions.LoadFromCache();
                 }
-                VersionList.DataContext = _versions;
-            }
-            catch (Exception)
+                catch (Exception e)
+                {
+                    Debug.WriteLine("List cache load failed:\n" + e.ToString());
+                }
+            });
+
+
+        }
+
+        public ICommand LaunchCommand => new RelayCommand((v) => InvokeLaunch((Version)v));
+        public ICommand InstallCommand => new RelayCommand((v) => InvokeInstall((Version)v));
+        public ICommand UninstallCommand => new RelayCommand((v) => InvokeUninstall((Version)v));
+
+        private void InvokeLaunch(Version v)
+        {
+            MessageBox.Show("InvokeLaunch");
+        }
+
+        private void InvokeInstall(Version v)
+        {
+            MessageBox.Show("InvokeInstall");
+            CancellationTokenSource cancelSource = new CancellationTokenSource();
+            v.DownloadInfo = new VersionDownloadInfo
             {
-            }
+                IsInitializing = true,
+                CancelCommand = new RelayCommand((o) => cancelSource.Cancel())
+            };
+            //TODO - Implement actual downloading
+            string dl_path = "Minecraft-" + v.Name;
+            Directory.CreateDirectory(dl_path);
+            v.DownloadInfo = null;
+            v.UpdateInstallStatus();
+        }
+
+        private void InvokeUninstall(Version v)
+        {
+            MessageBox.Show("InvokeUninstall");
+            Directory.Delete(v.GameDirectory, true);
+            v.UpdateInstallStatus();
         }
     }
 
-    public class Foo
+    namespace WPFDataTypes
     {
-        public string Name { get; set; }
-        public string Type { get; set; }
+        public class NotifyPropertyChangedBase : INotifyPropertyChanged
+        {
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void OnPropertyChanged(string name)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+
+        }
+
+        public interface IVersionCommands
+        {
+            ICommand LaunchCommand { get; }
+
+            ICommand InstallCommand { get; }
+
+            ICommand UninstallCommand { get; }
+        }
+
+        public class Version : NotifyPropertyChangedBase
+        {
+            public Version(string name, string type, IVersionCommands commands)
+            {
+                this.Name = name;
+                this.Type = type;
+                this.LaunchCommand = commands.LaunchCommand;
+                this.InstallCommand = commands.InstallCommand;
+                this.UninstallCommand = commands.UninstallCommand;
+            }
+
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public string GameDirectory => "Minecraft-" + Name;
+            public bool IsInstalled => Directory.Exists(GameDirectory);
+
+            public ICommand LaunchCommand { get; set; }
+            public ICommand InstallCommand { get; set; }
+            public ICommand UninstallCommand { get; set; }
+
+            private VersionDownloadInfo _downloadInfo;
+            public VersionDownloadInfo DownloadInfo
+            {
+                get { return _downloadInfo; }
+                set { _downloadInfo = value; OnPropertyChanged("DownloadInfo"); OnPropertyChanged("IsDownloading"); }
+            }
+
+            public bool IsDownloading => DownloadInfo != null;
+
+            public void UpdateInstallStatus()
+            {
+                OnPropertyChanged("IsInstalled");
+            }
+
+        }
+
+        public class VersionDownloadInfo : NotifyPropertyChangedBase
+        {
+
+            private bool _isInitializing;
+            private bool _isExtracting;
+            private long _downloadedBytes;
+            private long _totalSize;
+
+            public bool IsInitializing
+            {
+                get { return _isInitializing; }
+                set { _isInitializing = value; OnPropertyChanged("IsProgressIndeterminate"); OnPropertyChanged("DisplayStatus"); }
+            }
+
+            public bool IsExtracting
+            {
+                get { return _isExtracting; }
+                set { _isExtracting = value; OnPropertyChanged("IsProgressIndeterminate"); OnPropertyChanged("DisplayStatus"); }
+            }
+
+            public bool IsProgressIndeterminate
+            {
+                get { return IsInitializing || IsExtracting; }
+            }
+
+            public long DownloadedBytes
+            {
+                get { return _downloadedBytes; }
+                set { _downloadedBytes = value; OnPropertyChanged("DownloadedBytes"); OnPropertyChanged("DisplayStatus"); }
+            }
+
+            public long TotalSize
+            {
+                get { return _totalSize; }
+                set { _totalSize = value; OnPropertyChanged("TotalSize"); OnPropertyChanged("DisplayStatus"); }
+            }
+
+            public string DisplayStatus
+            {
+                get
+                {
+                    if (IsInitializing)
+                        return "Downloading...";
+                    if (IsExtracting)
+                        return "Extracting...";
+                    return "Downloading... " + (DownloadedBytes / 1024 / 1024) + "MiB/" + (TotalSize / 1024 / 1024) + "MiB";
+                }
+            }
+
+            public ICommand CancelCommand { get; set; }
+
+        }
     }
 }
---
+++ SmokeyVersionSwitcher/VersionList.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SmokeyVersionSwitcher
+{
+    class VersionList : ObservableCollection<WPFDataTypes.Version>
+    {
+        private readonly string _cache_file;
+        private readonly WPFDataTypes.IVersionCommands _commands;
+
+        public VersionList(string cache_file, WPFDataTypes.IVersionCommands commands)
+        {
+            _cache_file = cache_file;
+            _commands = commands;
+        }
+
+        private void ParseList(JArray data)
+        {
+            Clear();
+            // ([name, uuid, isBeta])[]
+            foreach (JObject keys in data)
+            {
+                Add(new WPFDataTypes.Version((string)keys["Name"], (string)keys["Type"], _commands));
+            }
+        }
+
+        public async Task LoadFromCache()
+        {
+            try
+            {
+                JArray jArray = JsonConvert.DeserializeObject<JArray>(File.ReadAllText(_cache_file));
+                ParseList(jArray);
+            }
+            catch (FileNotFoundException)
+            { // ignore
+            }
+        }
+    }
+}
